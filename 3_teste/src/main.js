0738b7db4a9e154cb26b7301b9af639b900c68e8
Bruno  Henrique Brandl <74260274+bruno21637@users.noreply.github.com>
2022-04-29 12:59:25 -0300

    .

--- 3_teste/src/main.js
+++ 3_teste/src/main.js
@@ -1,26 +1,13 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App2 from './App.vue'
 import VueRouter from 'vue-router'
 import componente1 from './components/componente1.vue';
 
 
-Vue.use(VueRouter);
 
-const router = new VueRouter({
-    routes: [
-        {
-            path: '/App',
-            component: componente1
-        }
-    ]
-})
-
-Vue.config.productionTip = true
-
-new Vue({
+new ({
     router,
-    render: h =>(App),
-
-
+    render: App2 =>(App2),
+        
     
-}).$mount('#App')
+}).$mount('#App2')
