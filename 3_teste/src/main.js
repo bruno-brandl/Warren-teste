ad4b38002780d89accb545005526833cf9d08b8a
Bruno  Henrique Brandl <74260274+bruno21637@users.noreply.github.com>
2022-04-29 17:16:40 -0300

    .

--- 3_teste/src/main.js
+++ 3_teste/src/main.js
@@ -1,13 +1,4 @@
 import { createApp } from 'vue'
-import App2 from './App.vue'
-import VueRouter from 'vue-router'
-import componente1 from './components/componente1.vue';
+import App from './App.vue'
 
-
-
-new ({
-    router,
-    render: App2 =>(App2),
-        
-    
-}).$mount('#App2')
+createApp(App).mount('#App')
